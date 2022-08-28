735556d354a69d42aa07f202217adeee5515955e
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-28 18:15:07 +0200

    more practice methods and patterns

--- app.js
+++ app.js
@@ -9,4 +9,28 @@ mainHeading.appendChild(excitedText);
 otherHeading.appendChild(excitedText);
 
 
+// Creating a New Community Content
+
+const communitySection = document.querySelector('.testimonials');
+
+const adviceHeading = document.createElement('h2');
+
+adviceHeading.innerHTML = "<strong><em>Advice For New Udacity Students</em><strong>";
+
+communitySection.appendChild(adviceHeading);
+
+const adviceSentence = document.createElement('p');
+adviceSentence.textContent = "Do Not Give Up! Just Keep Goind!!";
+
+adviceHeading.insertAdjacentElement("afterend",adviceSentence);
+
+// Three Ways to Remove an Element
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
 
