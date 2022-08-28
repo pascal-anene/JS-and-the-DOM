bf3246294dc6703f9b356fd671b85cadc2e406f7
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-28 17:17:38 +0200

    practicing DOM selection and manipulation methods

---
+++ app.js
@@ -0,0 +1,12 @@
+// const nanodegreeCard = document.querySelector('.card');
+
+const mainHeading = document.querySelector('h1');
+const otherHeading = document.querySelector('h4');
+const excitedText = document.createElement('span');
+
+excitedText.textContent = '!!!';
+mainHeading.appendChild(excitedText);
+otherHeading.appendChild(excitedText);
+
+
+
